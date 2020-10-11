a135a66d37bfe0b37f92f36645212eeaca469ecc
Troy <tpt38@uclive.ac.nz>
2020-10-11 15:05:00 +1300

    changed spacing of message

--- gameover.h
+++ gameover.h
@@ -10,8 +10,8 @@
 #define MESSAGE_RATE 30
 #define PACER_RATE 200
 #define GAME_OVER_TEXT "  GAME OVER"
-#define WIN_TEXT "   YOU WIN"
-#define LOSE_TEXT "   YOU LOSE"
+#define WIN_TEXT "  YOU WIN"
+#define LOSE_TEXT "  YOU LOSE"
 #define OVER_CODE 9 // arbitrary as long sa it isn't 0,1,2,3,4 or 13
 
 // Stops game and displays game over
